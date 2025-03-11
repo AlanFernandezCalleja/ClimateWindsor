5fd27acfad61bb62fe88d8140e2773db329865fd
Alan <alan.fernandez@ucb.edu.bo>
2025-03-11 13:18:27 -0400

    Creando la pantalla inicial de login

---
+++ frontend/static/js/script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function() {
+    const loginContainer = document.querySelector(".login-container");
+    loginContainer.style.animation = "zoomIn 0.5s ease-out forwards"; // Activa la animación después de que la página cargue
+});
